6579315743cca93bbe0e8c6d8953cc562598b64f
Sylvain Viole <sylvain.viole@yousign.com>
2022-10-11 00:02:32 +0200

    add error check on key parameter

--- src/cyStoreCommands.js
+++ src/cyStoreCommands.js
@@ -1,12 +1,18 @@
 let dataStorage = {};
 
 Cypress.Commands.add("store", (key, value, options) => {
+  if (!key) throw TypeError(`Missing "key" parameter`);
+  if (typeof key !== "string")
+    throw TypeError(`"key" parameter must be of type "string"`);
   options?.log && cy.log(`Storing ${key} = ${value}`);
   dataStorage[`${key}`] = value;
 });
 
 Cypress.Commands.add("getStored", (key, options) => {
-  options?.log && cy.log(`Getting stored value ${key} = ${value}`);
+  if (!key) throw TypeError(`Missing "key" parameter`);
+  if (typeof key !== "string")
+    throw TypeError(`"key" parameter must be of type "string"`);
+    options?.log && cy.log(`Getting stored value ${key} = ${value}`);
   return cy.wrap(dataStorage[`${key}`], { log: false });
 });
 
